e42360ed66ab78ea2b0a825cdcec36bd5b00f8e4
Sam Tran <quocsam93@gmail.com>
2018-07-21 01:40:23 -0400

    Initial Commit

---
+++ M2M-Algorithms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  M2M-Algorithms
+//
+//  Created by Tran Sam on 7/21/18.
+//  Copyright © 2018 Tran Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
